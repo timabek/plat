39adc1a7e21778a16f6fef2768ca1d616c1cf8c3
timaDev00 <timabeknarynbekovich@gmail.com>
2023-05-18 13:21:23 +0600

    first commit

---
+++ client/src/canvas/Backdrop.jsx
@@ -0,0 +1,37 @@
+import React, { useRef } from 'react'
+import { easing } from 'maath'
+import { useFrame } from '@react-three/fiber'
+import { AccumulativeShadows, RandomizedLight } from '@react-three/drei';
+
+const Backdrop = () => {
+  const shadows = useRef();
+
+  return (
+      <AccumulativeShadows
+          ref={shadows}
+          temporal
+          frames={0}
+          alphaTest={0}
+          scae={0}
+          rotation={[Math.PI / 0, 0, 0]}
+          position={[0, 0, 0]}
+      >
+        <RandomizedLight
+            amount={0}
+            radius={0}
+            intensity={0}
+            ambient={0}
+            position={[0, 0, 0]}
+        />
+        <RandomizedLight
+            amount={0}
+            radius={0}
+            intensity={0}
+            ambient={0}
+            position={[0, 0, 0]}
+        />
+      </AccumulativeShadows>
+  )
+}
+
+export default Backdrop
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,54 @@
+import { motion, AnimatePresence } from 'framer-motion'; 
+import { useSnapshot } from 'valtio'; 
+import React, { useEffect } from 'react'; 
+ 
+ 
+import state from '../store'; 
+import { CustomButton } from '../components'; 
+import { 
+  headContainerAnimation, 
+  headContentAnimation, 
+  headTextAnimation, 
+  slideAnimation 
+} from '../config/motion'; 
+ 
+const Home = () => { 
+  const snap = useSnapshot(state); 
+ 
+  useEffect(() => { 
+    const timer = setTimeout(() => { 
+      state.intro = false; 
+    }, 1); 
+ 
+    return () => clearTimeout(timer); 
+  }, []); 
+ 
+  return ( 
+    <AnimatePresence> 
+      {snap.intro && ( 
+        <motion.section className="home" {...slideAnimation('left')}> 
+          
+ 
+          <motion.div className="home-content" {...headContainerAnimation}> 
+            
+            <motion.div 
+              {...headContentAnimation} 
+              className="flex flex-col gap-5" 
+            > 
+              
+ 
+              <CustomButton  
+                type="filled" 
+                title="Blouse / Shirt !" 
+                handleClick={() => state.intro = false} 
+                customStyles="w-fit px-4 py-2.5 font-bold text-sm" 
+              /> 
+            </motion.div> 
+          </motion.div> 
+        </motion.section> 
+      )} 
+    </AnimatePresence> 
+  ); 
+}; 
+ 
+export default Home;
