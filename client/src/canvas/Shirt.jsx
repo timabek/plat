2284e0b54491997db2a722d5a9485d8676f664c9
timaDev00 <timabeknarynbekovich@gmail.com>
2023-05-18 14:08:27 +0600

    add that

--- client/src/canvas/Shirt.jsx
+++ client/src/canvas/Shirt.jsx
@@ -8,7 +8,7 @@ import state from '../store';
 
 const Shirt = () => {
   const snap = useSnapshot(state);
-  const { nodes, materials } = useGLTF('/plat.glb');
+  const { nodes, materials } = useGLTF('/plats.glb');
 
   const logoTexture = useTexture(snap.logoDecal);
   const fullTexture = useTexture(snap.fullDecal);
